7c8d100de4d187ec80e52937282c9b01d8aa17da
benjaminvaughan <vaughanb18@gmail.com>
2019-02-25 01:31:22 -0800

    added documentation for the parts file

--- radio telescope/parts.py
+++ radio telescope/parts.py
@@ -22,9 +22,20 @@ class Encoder():
         self.position = 0
 
     def call_back_a(self, pin, level, tick):
+        """
+        Purpose: The purpose of this function is to provide a callback to be called within pigpios callback function. It sets the value of a_state to that of the level produced by the encoder at the time of the callback
+        Inputs: None
+        Outputs: None
+        Original Author: Benjamin Vaughan
+        """
         self.a_state = level
 
     def call_back_b(self, pin, level, tick):
+        """
+        The purpose of this function is to provide a callback to be called within the pigpio callback function. It tests to see if the a_state is equal to 1 or 0 then incrememnts the position accordingly. Adittionally it converts from increments of position to degrees
+        inputs: None
+        Outputs: None
+        """
         if self.a_state:
             self.position += 1
             self.direction = "clockwise"
@@ -39,9 +50,22 @@ class Encoder():
             self.degrees -= 360
 
     def get_degrees(self):
+        """
+        Purpose: This function is used to record the current degrees determined from the encoder's ticks
+        Inputs: none
+        Outputs:
+        self.degrees - the number of degrees the motor has turned
+        Original Author: Benjamin Vaughan
+        """
         return float(self.degrees)
 
     def run_encoder(self):
+        """
+        The purpose of this function is to call the callback functions defined by pigpio. This function is needed in order to run the encoders
+        Inputs: None
+        Outputs: None
+        Original Author: Benjamin Vaughan
+        """
         self.pi.callback(self.a_pin, 2, self.call_back_a)
         self.pi.callback(self.b_pin, 1, self.call_back_b)
 
@@ -65,30 +89,73 @@ class Motor():
                            "1/16" : (0,0,1),
                            "1/32" : (1,0,1)}
     def a32_msteps(self):
+        """
+        The purpose of this function is to set the motor to 32 microstepping mode
+        inputs: none
+        outputs: none
+        Original Author: Benjamin Vaughan
+        """
         for i in range(3):
             self.pi.write(self.mode[i], self.resolution["1/32"][i])
 
     def a16_msteps(self):
+        """
+        The purpose of this function is to set the motor to 16 microstepping mode
+        inputs: none
+        outputs: none
+        Original Author: Benjamin Vaughan
+        """
         for i in range(3):
             self.pi.write(self.mode[i], self.resolution["1/16"][i])
 
     def a8_msteps(self):
+        """
+        The purpose of this function is to set the motor to 8 microstepping mode
+        inputs: none
+        outputs: none
+        Original Author: Benjamin Vaughan
+        """
         for i in range(3):
             self.pi.write(self.mode[i], self.resolution["1/8" ][i])
 
     def a4_msteps(self):
+        """
+        The purpose of this function is to set the motor to 4 microstepping mode
+        inputs: none
+        outputs: none
+        Original Author: Benjamin Vaughan
+        """
         for i in range(3):
             self.pi.write(self.mode[i], self.resolution["1/4" ][i])
 
     def half_step(self):
+        """
+        The purpose of this function is to set the motor to 2 microstepping mode
+        inputs: none
+        outputs: none
+        Original Author: Benjamin Vaughan
+        """
         for i in range(3):
             self.pi.write(self.mode[i], self.resolution["Half"][i])
 
     def full_step(self):
+        """
+        The purpose of this function is to set the motor to full step mode
+        inputs: none
+        outputs: none
+        Original Author: Benjamin Vaughan
+        """
         for i in range(3):
             self.pi.write(self.mode[i], self.resolution["Full"][i])
 
     def set_speed(self, speed):
+        """
+        The purpose of this function is to check the speed input and set the motor to the matching speed
+        Inputs: 
+        Speed - the desired speed you want to run at range(0-7)
+        outputs: None
+        Original Author: Benjamin Vaughan
+        """
         if speed == 0:
             self.stop_motor()
         elif speed == 1:
@@ -114,14 +181,35 @@ class Motor():
             print("Haha")
 
     def set_frequency_dutycycle(self, dutycycle, frequency):
+        """
+        The purpose of this function is to set the frequency and duty cycle to be sent to the motor controller from the raspberry pi
+        Inputs: 
+        Dutycycle - the desired dutycycle
+        Frequency - the desired frequency
+        Outputs: None
+        Original Author: Benjamin Vaughan
+        """
         self.pi.set_PWM_dutycycle(self.s_pin, dutycycle)
         self.pi.set_PWM_frequency(self.s_pin, frequency)
 
     def stop_motor(self):
+        """
+        The purpose of this function is to stop the motor from moving
+        Inputs: None
+        Outputs: None
+        Original Author : Benjamin Vaughan
+        """
         self.pi.set_PWM_dutycycle(self.s_pin, 0)
         self.pi.set_PWM_frequency(self.s_pin, 0)
 
     def set_direction(self, direction):
+        """
+        The purpose of this function is to set the direction of the motor to either clockwise or counterclockwise
+        Inputs: 
+        direction - (0 or 1) 1 = clockwise, 0 = counterclockwise
+        Outputs: None
+        Original Author : Benjamin Vaughan
+        """
         self.pi.write(self.d_pin, direction)
 
 if __name__ == "__main__":
